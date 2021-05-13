d444f6ec9582adb47784591dbe14136884505759
buketyagmuraltin <buketyagmuraltin@hotmail.com>
2021-05-13 18:24:44 +0300

    -day4 hw2: final commit

---
+++ interfaceAbstractDemo/src/Adapters/MernisServiceAdapter.java
@@ -0,0 +1,26 @@
+package Adapters;
+
+import java.rmi.RemoteException;
+
+import Abstract.CustomerCheckService;
+import Entities.Customer;
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
+
+public class MernisServiceAdapter implements CustomerCheckService {
+
+	@Override
+	public boolean checkIfRealPerson(Customer customer) {
+
+		KPSPublicSoapProxy client = new KPSPublicSoapProxy();
+
+		try {
+			boolean checker =  client.TCKimlikNoDogrula(Long.parseLong(customer.getNationalityId()), customer.getFirstName().toUpperCase(),
+					customer.getLastName().toUpperCase(), customer.getDateOfBirth());
+			return checker;
+		} catch (RemoteException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+
+}
